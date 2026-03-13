7cbdf3143bb593326d9c904eb1ba7c04618795e1
Genesis Belmonte <genesisbelmonte4@gmail.com>
2026-03-13 12:10:17 -0700

    Add route to create a new snippet

--- main.go
+++ main.go
@@ -13,10 +13,15 @@ func snippetView(w http.ResponseWriter, r *http.Request){
 	w.Write([]byte("Display a specific snippet..."))
 }
 
+func snippetCreate(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Display a for for creating a new snippet..."))
+}
+
 func main(){
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("starting server on :4000")
 
