233f22ff61f83cc1b758edf466fcbe660159a3eb
Genesis Belmonte <genesisbelmonte4@gmail.com>
2026-03-13 12:08:47 -0700

    Add snippetview route

--- main.go
+++ main.go
@@ -9,9 +9,14 @@ func home(w http.ResponseWriter, r *http.Request){
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Display a specific snippet..."))
+}
+
 func main(){
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
 
 	log.Print("starting server on :4000")
 
